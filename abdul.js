cd6fb30a0200124ea8ee569e706082000f93df54
icedlo <munyaomusyimi10@gmail.com>
2023-04-09 10:09:29 +0300

    javascript

---
+++ abdul.js
@@ -0,0 +1,74 @@
+/*
+console.log("my first javascript programbbbb");
+console.log("hello world");
+
+
+pi=7.88
+console.log(pi)
+
+let name ="aaron";
+console.log(name);
+
+var Isabdul= true;
+if (Isabdul){
+    console.log('its abdul');
+} else{
+    console.log('its not abdul');
+}
+
+
+var numbers = [1,2,3,4,5,6];
+console.log(numbers[0]);
+*/
+/*
+let myName="abdul";
+let last_name="eeb";
+console.log("my name is "+ myName + last_name);
+*/
+/*
+var myName="abdul";
+var last_name="eeb";
+console.log("my name is "+ myName + last_name);
+
+let w=34;
+let e=4;
+console.log(w+e);
+console.log(w%e);
+console.log(w==e); 
+*/
+/*
+const sum=function(i,j){
+    return i+j;
+
+}
+let i=4;
+let j=4;
+console.log(sum(i,j));
+*/
+
+
+/*
+const details=function(nam,age ,course){
+return `my name is ${nam} i am ${age} years and im doing ${course}`
+};
+let nam="abdul";
+let age= 20;
+let course="computerscience";
+
+console.log(details(nam,age,course));
+*/
+
+
+//anonymous function
+
+
+
+function Hi(name){
+    console.log(`hello ${name} , how are you buddy`);
+}
+    Hi("abdul");
+    Hi("Daniel");
+
+    
+
+
